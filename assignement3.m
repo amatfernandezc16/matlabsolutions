66e63d7ce0bdaabd3c91cf90a6675e66d8701ad8
amatfernandezc16 <amatfernandezc16@students.ecu.edu>
2016-09-14 18:26:46 -0400

    Solutions for Assignment 3

---
+++ assignement3.m
@@ -0,0 +1,8 @@
+%Clara Amat Fernandez
+%9/12/2016
+%This code allows the user to play Tic Tac Toe
+input('Welcome to the greatest Tic Tac Toe game!')
+input('A board of zeros will be displayed in the command window')
+input('When it is your turn to play, change the zero where you want to play \nfor a 1')
+disp(tictac)
+tictac(2, 2)=2
